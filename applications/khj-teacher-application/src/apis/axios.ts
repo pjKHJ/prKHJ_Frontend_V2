ba9979f45bfed560eafa161ea0ddfc38885b5ac0
jaejun <ssokage090210@naver.com>
2026-04-14 10:25:30 +0900

    add env
    add env

--- applications/khj-teacher-application/src/apis/axios.ts
+++ applications/khj-teacher-application/src/apis/axios.ts
@@ -1,6 +1,14 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
+if (!baseURL) {
+  throw new Error(
+    "VITE_API_BASE_URL is not defined. Please set VITE_API_BASE_URL in your environment.",
+  );
+}
+
 export const api = axios.create({
-  baseURL: "http://localhost:8080", // 예시
+  baseURL,
   withCredentials: true,
 });

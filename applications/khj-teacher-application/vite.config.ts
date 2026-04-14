1117812e470b2c2a6e71b8d917ee230ac21a3b2f
jaejun <ssokage090210@naver.com>
2026-04-14 10:17:08 +0900

    pwa
    pwa

--- applications/khj-teacher-application/vite.config.ts
+++ applications/khj-teacher-application/vite.config.ts
@@ -1,10 +1,40 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "KHJ App",
+        short_name: "KHJ",
+        description: "KHJ Teacher App",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "minimal-ui",
+        start_url: "/",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+
   resolve: {
     alias: {
       "@khj/hooks": path.resolve(__dirname, "../../modules/khj-hooks/index.ts"),
